a4ea8fceb09ff845ab564fe4bb3565942291bb4b
Thang Nguyen <thangnguyenuvm@gmail.com>
2024-03-30 17:33:14 -0400

    Read from native config file

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,93 @@
+use std::{error::Error, fs::{self, File, read_to_string}, io::{ErrorKind, Read}, path::{Path, PathBuf}};
+use log::{info, trace};
 use serde::{Serialize, Deserialize};
+use directories::ProjectDirs;
+
+pub static HOSTS_FILE_PATH: &str = "/tmp/hosts";
+pub static HOSTS_GROUP_CONFIG_PATH: &str = "/tmp/refocus_host_group_config.json";
+pub static HOSTNAME_ANCHOR: &str = "refocus.dev";
+pub static APPNAME: &str = "refocus";
+pub static HOSTNAME_GROUPS_CONFIG_FILENAME: &str = "refocus_hostnames_group.json";
 
 pub type HostnameGroups = Vec<HostnameGroup>;
 
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct HostnameGroup {
     pub name: String,
     pub hostnames: Vec<String>,
+}
+
+impl std::default::Default for HostnameGroup
+{
+    fn default() -> Self {
+        Self { 
+            name: String::from("Social Media"),
+            hostnames: vec!["facebook.com".to_string(), "instagram.com".to_string(), "twitter.com".to_string(), "linkedin.com".to_string()]
+        }
+    }
+}
+
+pub fn get_config_file_dir() -> Result<PathBuf, Box<dyn Error>>
+{
+    let project_dirs = ProjectDirs::from("rs", APPNAME, APPNAME).unwrap();
+    let path = project_dirs.config_dir().join(Path::new(HOSTNAME_GROUPS_CONFIG_FILENAME));
+    Ok(path)
+}
+
+pub fn read_hosts() -> Result<String, Box<dyn Error>>
+{
+    let hosts_content = read_to_string(HOSTS_FILE_PATH)?;
+    Ok(hosts_content)
+}
+
+pub fn read_hostname_groups_config() -> Result<HostnameGroups, Box<dyn Error>>
+{
+    let path = get_config_file_dir()?;
+
+    println!("Config path: {:?}", path);
+
+    match File::open(&path)
+    {
+        Ok(mut file) => {
+            let mut buf = String::new();
+            file.read_to_string(&mut buf)?;
+            let hostname_groups = serde_json::from_str(&buf)?;
+            Ok(hostname_groups)
+        },
+        Err(ref e) if e.kind() == ErrorKind::NotFound => {
+            let parent = path.parent().unwrap();
+            fs::create_dir_all(parent)?;
+            let hostname_groups = vec![
+                HostnameGroup::default()
+            ];
+            let content = serde_json::to_string_pretty(&hostname_groups)?;
+            fs::write(path, content)?;
+            Ok(hostname_groups)
+        },
+        Err(e) => Err(e.into())
+    }
+}
+ 
+pub fn construct_refocus_line(hostgroups: &HostnameGroups) -> String
+{
+    info!("Generating Refocus hosts line");
+    let mut refocus_line = String::from("127.0.0.1 ".to_owned() + HOSTNAME_ANCHOR + " ");
+    for group in hostgroups
+    {
+        for hostname in &group.hostnames
+        {
+            refocus_line.push_str(hostname);
+            refocus_line.push_str(" ");
+            if !hostname.contains("www.")
+            {
+                let w = "www.".to_string() + hostname;
+                refocus_line.push_str(&w);
+                refocus_line.push_str(" ");
+            }
+        }
+    }
+    refocus_line.push_str("\n");
+
+    trace!("Refocus line: {:?}", refocus_line);
+    refocus_line
 }
--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,10 @@
-use std::{error::Error, fs::{self, read_to_string}};
+use std::fs;
 use log::{info, trace};
 use refocus::*;
 
-static HOSTS_FILE_PATH: &str = "/tmp/hosts";
-static HOSTS_GROUP_CONFIG_PATH: &str = "/tmp/refocus_host_group_config.json";
-static HOSTNAME_ANCHOR: &str = "refocus.dev";
-
-fn read_hosts() -> Result<String, Box<dyn Error>>
-{
-    let hosts_content = read_to_string(HOSTS_FILE_PATH)?;
-    Ok(hosts_content)
-}
-
-fn read_hostname_group_config() -> Result<HostnameGroups, Box<dyn Error>>
-{
-    let hostgroup_config = read_to_string(HOSTS_GROUP_CONFIG_PATH)?;
-    let hostgroups: HostnameGroups = serde_json::from_str(&hostgroup_config).unwrap();
-
-    Ok(hostgroups)
-}
-
-fn construct_refocus_line(hostgroups: &HostnameGroups) -> String
-{
-    info!("Generating Refocus hosts line");
-    let mut refocus_line = String::from("127.0.0.1 ".to_owned() + HOSTNAME_ANCHOR + " ");
-    for group in hostgroups
-    {
-        for hostname in &group.hostnames
-        {
-            refocus_line.push_str(hostname);
-            refocus_line.push_str(" ");
-            if !hostname.contains("www.")
-            {
-                let w = "www.".to_string() + hostname;
-                refocus_line.push_str(&w);
-                refocus_line.push_str(" ");
-            }
-        }
-    }
-    refocus_line.push_str("\n");
-
-    trace!("Refocus line: {:?}", refocus_line);
-    refocus_line
-}
-
 fn main() {
     let mut hosts_content = read_hosts().unwrap();
-    let hostgroups = read_hostname_group_config().unwrap();
-
+    let hostgroups = read_hostname_groups_config().unwrap();
 
     if hosts_content.contains(HOSTNAME_ANCHOR)
     {
