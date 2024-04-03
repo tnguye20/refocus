080fbae657fede69048697e984d88c222bde2831
Thang Nguyen <thangnguyenuvm@gmail.com>
2024-04-02 20:51:34 -0400

    Resolved root issue

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,12 @@
-use std::{error::Error, fs::{self, File, read_to_string}, io::{ErrorKind, Read}, path::{Path, PathBuf}};
-use log::{info, trace};
-use serde::{Serialize, Deserialize};
 use directories::ProjectDirs;
+use log::{info, trace};
+use serde::{Deserialize, Serialize};
+use std::{
+    error::Error,
+    fs::{self, read_to_string, File},
+    io::{ErrorKind, Read},
+    path::{Path, PathBuf},
+};
 
 pub static APPNAME: &str = "refocus";
 pub static HOSTS_FILE_PATH: &str = "/etc/hosts";
@@ -17,84 +22,93 @@ pub struct HostnameGroup {
     pub hostnames: Vec<String>,
 }
 
-impl std::default::Default for HostnameGroup
-{
+impl std::default::Default for HostnameGroup {
     fn default() -> Self {
-        Self { 
+        Self {
             name: String::from("Social Media"),
-            hostnames: vec!["facebook.com".to_string(), "instagram.com".to_string(), "twitter.com".to_string(), "linkedin.com".to_string()]
+            hostnames: vec![
+                "facebook.com".to_string(),
+                "instagram.com".to_string(),
+                "twitter.com".to_string(),
+                "linkedin.com".to_string(),
+            ],
         }
     }
 }
 
-pub fn create_tmp_hosts_file() -> Result<(), Box<dyn Error>>
-{
+pub fn create_tmp_hosts_file() -> Result<(), Box<dyn Error>> {
     let hosts_content = read_to_string(HOSTS_FILE_PATH)?;
-    fs::write(TMP_HOSTS_FILE_PATH, &hosts_content)?;
+    fs::write(TMP_HOSTS_FILE_PATH, hosts_content)?;
     Ok(())
 }
 
-pub fn get_config_file_dir() -> Result<PathBuf, Box<dyn Error>>
-{
+pub fn get_config_file_dir() -> Result<PathBuf, Box<dyn Error>> {
     let project_dirs = ProjectDirs::from("rs", APPNAME, APPNAME).unwrap();
-    let path = project_dirs.config_dir().join(Path::new(HOSTNAME_GROUPS_CONFIG_FILENAME));
+    let path = project_dirs
+        .config_dir()
+        .join(Path::new(HOSTNAME_GROUPS_CONFIG_FILENAME));
     Ok(path)
 }
 
-pub fn read_hosts() -> Result<String, Box<dyn Error>>
-{
+pub fn read_hosts() -> Result<String, Box<dyn Error>> {
     let hosts_content = read_to_string(HOSTS_FILE_PATH)?;
     Ok(hosts_content)
 }
 
-pub fn read_hostname_groups_config() -> Result<HostnameGroups, Box<dyn Error>>
-{
+pub fn read_hostname_groups_config() -> Result<HostnameGroups, Box<dyn Error>> {
     let path = get_config_file_dir()?;
 
     println!("Config path: {:?}", path);
 
-    match File::open(&path)
-    {
+    match File::open(&path) {
         Ok(mut file) => {
             let mut buf = String::new();
             file.read_to_string(&mut buf)?;
             let hostname_groups = serde_json::from_str(&buf)?;
             Ok(hostname_groups)
-        },
+        }
         Err(ref e) if e.kind() == ErrorKind::NotFound => {
             let parent = path.parent().unwrap();
             fs::create_dir_all(parent)?;
-            let hostname_groups = vec![
-                HostnameGroup::default()
-            ];
+            let hostname_groups = vec![HostnameGroup::default()];
             let content = serde_json::to_string_pretty(&hostname_groups)?;
             fs::write(path, content)?;
             Ok(hostname_groups)
-        },
-        Err(e) => Err(e.into())
+        }
+        Err(e) => Err(e.into()),
     }
 }
- 
-pub fn construct_refocus_line(hostgroups: &HostnameGroups) -> String
-{
+
+pub fn construct_refocus_line(hostgroups: &HostnameGroups) -> String {
     info!("Generating Refocus hosts line");
-    let mut refocus_line = String::from("127.0.0.1 ".to_owned() + HOSTNAME_ANCHOR + " ");
-    for group in hostgroups
-    {
-        for hostname in &group.hostnames
-        {
+    let mut refocus_line = "127.0.0.1 ".to_owned() + HOSTNAME_ANCHOR + " ";
+    for group in hostgroups {
+        for hostname in &group.hostnames {
             refocus_line.push_str(hostname);
-            refocus_line.push_str(" ");
-            if !hostname.contains("www.")
-            {
+            refocus_line.push(' ');
+            if !hostname.contains("www.") {
                 let w = "www.".to_string() + hostname;
                 refocus_line.push_str(&w);
-                refocus_line.push_str(" ");
+                refocus_line.push(' ');
             }
         }
     }
-    refocus_line.push_str("\n");
+    refocus_line.push('\n');
 
     trace!("Refocus line: {:?}", refocus_line);
     refocus_line
+}
+
+pub fn copy_to_etc() -> Result<(), Box<dyn Error>> {
+    let out = std::process::Command::new("sudo")
+        .arg("cp")
+        .arg(TMP_HOSTS_FILE_PATH)
+        .arg(HOSTS_FILE_PATH)
+        .output()?;
+
+    if !out.status.success() {
+        panic!("Failed to copy to /etc/hosts");
+    }
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,33 @@
-use std::{fs, process};
 use log::{info, trace};
 use refocus::*;
+use std::{fs, process};
 
 fn main() {
-
     info!("Copy from {} to {}", HOSTS_FILE_PATH, TMP_HOSTS_FILE_PATH);
 
-    if let Err(_) = create_tmp_hosts_file()
-    {
+    if create_tmp_hosts_file().is_err() {
         eprintln!("Insufficient permission to backup {}", HOSTS_FILE_PATH);
         process::exit(1);
     }
     let mut hosts_content = read_hosts().unwrap();
     let hostgroups = read_hostname_groups_config().unwrap();
 
-    if hosts_content.contains(HOSTNAME_ANCHOR)
-    {
+    if hosts_content.contains(HOSTNAME_ANCHOR) {
         info!("Refocus Line found. Finding line to regenerate");
         let mut new_hosts_content = String::from("");
-        for line in hosts_content.lines()
-        {
-            if line.contains(HOSTNAME_ANCHOR)
-            {
+        for line in hosts_content.lines() {
+            if line.contains(HOSTNAME_ANCHOR) {
                 new_hosts_content.push_str(&construct_refocus_line(&hostgroups));
-                new_hosts_content.push_str("\n");
-            }
-            else
-            {
+                new_hosts_content.push('\n');
+            } else {
                 new_hosts_content.push_str(line);
-                new_hosts_content.push_str("\n");
+                new_hosts_content.push('\n');
             }
         }
         trace!("New hosts content: \n {:?}", new_hosts_content);
 
         fs::write(TMP_HOSTS_FILE_PATH, new_hosts_content).expect("Unable to write hosts file");
-    }
-    else
-    {
+    } else {
         info!("Refocus Line not found. Creating one and appending at end of file");
         hosts_content.push_str(&construct_refocus_line(&hostgroups));
         trace!("New hosts content: \n {:?}", hosts_content);
@@ -44,8 +35,7 @@ fn main() {
         fs::write(TMP_HOSTS_FILE_PATH, hosts_content).expect("Unable to write hosts file");
     }
 
-    if let Err(_) = fs::copy(TMP_HOSTS_FILE_PATH, HOSTS_FILE_PATH)
-    {
+    if copy_to_etc().is_err() {
         eprintln!("Insufficient permission to overwrite {}", HOSTS_FILE_PATH);
         process::exit(1);
     }
